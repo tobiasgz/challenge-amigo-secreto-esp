b81a1980158addc56f3739783ff469a0ac98c192
Tobias Gonzalez <gonzaleztobias09@gmail.com>
2025-03-12 19:09:05 -0300

    Agregada funcion sortear y generador de numero, se modifca funcion actualizar lista para cumplir consigna

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let listaAmigosJS = [];
 let listaAmigosHTML = document.getElementById("listaAmigos");
+let resultado = document.getElementById("resultado");
 
 function agregarAmigo() { // Tomar texto del html y agregarlo a la lista
     let amigo = document.getElementById("amigo").value;
@@ -9,10 +10,13 @@ function agregarAmigo() { // Tomar texto del html y agregarlo a la lista
 
 
 
-function actualizarLista(persona) {
-    const li = document.createElement("li"); // Crear un elemento <li>
-    li.textContent = persona; // Agregar el texto del elemento
-    listaAmigosHTML.appendChild(li); // Insertar el <li> en el <ul>
+function actualizarLista(persona) { // Se modifica para cumplir la consigna de usar ciclo + innerHTML
+    listaAmigosHTML.innerHTML = "";
+    listaAmigosJS.forEach(item => {
+        const li = document.createElement("li"); // Crear un elemento <li>
+        li.textContent = item; // Agregar el texto del elemento
+        listaAmigosHTML.appendChild(li); // Insertar el <li> en el <ul>
+    });
      
 }
 
@@ -25,3 +29,18 @@ function verificadorDeNombre(persona) { // Verificar que el campo no sea vacio
     }
 }
 
+function sortearAmigo() { // Solicita al ganador y lo muestra si es que hay una lista de amigos
+    if (listaAmigosJS.length !== 0) {
+        let numeroGanador = generadorDeNumero();
+        resultado.innerHTML = `El ganador es: ${listaAmigosJS[numeroGanador]}`;
+    } else {
+        alert("No hay amigos para sortear");
+    }
+    
+    
+}
+
+function generadorDeNumero() { // Genera al ganador
+    let numeroMaximo = listaAmigosJS.length;
+    return Math.floor(Math.random() * numeroMaximo); 
+}
