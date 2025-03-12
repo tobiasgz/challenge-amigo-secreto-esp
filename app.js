b4af6e539f91d3de53a2ec3c23f98e63ae439aa3
Tobias Gonzalez <gonzaleztobias09@gmail.com>
2025-03-12 18:14:27 -0300

    Agregada lista y boton para agregar a lista

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigos = [];
+
+function agregarAmigo() {
+    let amigo = document.getElementById("amigo").value;
+    listaAmigos.push(amigo);
+    actualizarLista();
+}
+
+
+
+function actualizarLista() {
+    console.log(listaAmigos);
+}
