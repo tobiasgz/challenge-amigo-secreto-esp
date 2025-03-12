97d994124113c095e6468a5d25d473109ace5e17
Tobias Gonzalez <gonzaleztobias09@gmail.com>
2025-03-12 18:32:49 -0300

    Agregada funcion de verificar campo vacio y actualizar lista

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let listaAmigos = [];
+let listaAmigosJS = [];
+let listaAmigosHTML = document.getElementById("listaAmigos");
 
-function agregarAmigo() {
+function agregarAmigo() { // Tomar texto del html y agregarlo a la lista
     let amigo = document.getElementById("amigo").value;
-    listaAmigos.push(amigo);
-    actualizarLista();
+    verificadorDeNombre(amigo);
 }
 
 
 
-function actualizarLista() {
-    console.log(listaAmigos);
+function actualizarLista(persona) {
+    const li = document.createElement("li"); // Crear un elemento <li>
+    li.textContent = persona; // Agregar el texto del elemento
+    listaAmigosHTML.appendChild(li); // Insertar el <li> en el <ul>
+     
 }
+
+function verificadorDeNombre(persona) { // Verificar que el campo no sea vacio
+    if (persona != '') {
+        listaAmigosJS.push(persona);
+        actualizarLista(persona);
+    } else {
+        alert("El nombre ingresado no es correcto");
+    }
+}
+
